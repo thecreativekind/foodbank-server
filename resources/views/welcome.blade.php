c770f156d7dfd8c16664a427936aef789c92dbab
Ben Carter <bencarter78@hotmail.com>
2017-09-10 17:16:18 +0100

    Updating home page

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -5,7 +5,7 @@
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>Laravel</title>
+        <title>{!! config('app.name') !!}</title>
 
         <!-- Fonts -->
         <link href="https://fonts.googleapis.com/css?family=Raleway:100,600" rel="stylesheet" type="text/css">
@@ -49,6 +49,10 @@
                 font-size: 84px;
             }
 
+            .sub-title {
+                font-size: 36px;
+            }
+
             .links > a {
                 color: #636b6f;
                 padding: 0 25px;
@@ -79,15 +83,11 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    {!! config('app.name') !!}
                 </div>
 
-                <div class="links">
-                    <a href="https://laravel.com/docs">Documentation</a>
-                    <a href="https://laracasts.com">Laracasts</a>
-                    <a href="https://laravel-news.com">News</a>
-                    <a href="https://forge.laravel.com">Forge</a>
-                    <a href="https://github.com/laravel/laravel">GitHub</a>
+                <div class="sub-title m-b-md">
+                    <small>Coming soon...</small>
                 </div>
             </div>
         </div>
